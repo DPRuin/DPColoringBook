3ad4e9af8424fe9f3dcc0f882b540195b058aa8a
DPRuin <dpruin@126.com>
2018-01-19 10:32:32 +0800

    creat ColorView

---
+++ DPColoringBook/DPColoringBook/ColoringView.swift
@@ -0,0 +1,21 @@
+//
+//  ColoringView.swift
+//  DPColoringBook
+//
+//  Created by 土老帽 on 2018/1/19.
+//  Copyright © 2018年 DPRuin. All rights reserved.
+//
+
+import UIKit
+
+class ColoringView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- DPColoringBook/DPColoringBook/ColoringViewController.swift
+++ DPColoringBook/DPColoringBook/ColoringViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ColoringViewController.swift
 //  DPColoringBook
 //
 //  Created by 土老帽 on 2018/1/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColoringViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
