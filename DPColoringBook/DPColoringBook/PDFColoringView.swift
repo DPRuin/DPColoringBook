85ed696861f2eb45b89d5d9eca8b7486badd982a
DPRuin <dpruin@126.com>
2018-01-25 12:05:05 +0800

    creat pdfcoloringview

---
+++ DPColoringBook/DPColoringBook/PDFColoringView.swift
@@ -0,0 +1,21 @@
+//
+//  PDFColoringView.swift
+//  DPColoringBook
+//
+//  Created by 土老帽 on 2018/1/25.
+//  Copyright © 2018年 DPRuin. All rights reserved.
+//
+
+import UIKit
+
+class PDFColoringView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
