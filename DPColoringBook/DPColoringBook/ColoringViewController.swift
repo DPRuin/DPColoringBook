be3f6dea7aef15295f08bf375e606de9cd5072bd
DPRuin <dpruin@126.com>
2018-01-25 15:19:38 +0800

    controller pdf

--- DPColoringBook/DPColoringBook/ColoringViewController.swift
+++ DPColoringBook/DPColoringBook/ColoringViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ColoringViewController: UIViewController {
 
-    @IBOutlet weak var coloringImageView: ColoringImageView!
+    var startPoint: CGPoint?
     
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,36 @@ class ColoringViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        startPoint = touches.first?.location(in:self.imageView )
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let point = touches.first?.location(in:self.imageView )
+        
+        drawLine(fromPoint: startPoint!, toPoint: point!)
+        startPoint = point
+    }
+    
+    func drawLine(fromPoint: CGPoint, toPoint: CGPoint) {
+        let path = Bundle.main.path(forResource: "test", ofType: "pdf")!
+        
+        UIGraphicsBeginPDFContextToFile(path, self.imageView.frame, nil)
+        self.imageView.draw(self.imageView.frame)
+        UIGraphicsBeginPDFPageWithInfo(self.imageView.frame, nil)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(10)
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        context.setLineCap(CGLineCap.round)
+        context.flush()
+        context.strokePath()
+        UIGraphicsEndPDFContext()
+        
     }
     
 
